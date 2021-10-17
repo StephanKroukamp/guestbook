86ac99cb9ed1b450246c12b8a062e00fb0e45155
Stephan <stephan.kroukamp@gmail.com>
2021-10-17 13:01:14 +0200

    init repo

---
+++ contracts/Guestbook.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.16;
+
+contract Guestbook {
+  event SignatureAdded(string message, bytes32 name);
+  bytes32[] guests;
+  function signBook(bytes32 name) public {
+    guests.push(name);
+    emit SignatureAdded("New guest signature!", name);
+  }
+  function getNames() public view returns (bytes32[] memory) {
+    return guests;
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div v-if="isDrizzleInitialized" id="app">
+    <h1>Sign the Guestbook</h1>
+    <drizzle-contract-form
+      contractName="Guestbook"
+      method="signBook"
+      :placeholders="['Name']"
+    />
+    <h2>Guests:</h2>
+    <ul v-if="getNames">
+      <li v-for="(name, i) in getNames" :key="i">{{ utils.toUtf8(name) }}</li>
+    </ul>
+  </div>
+  <div v-else>
+    Loading application...
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+export default {
+  name: "app",
+  computed: {
+    ...mapGetters("drizzle", ["drizzleInstance", "isDrizzleInitialized"]),
+    ...mapGetters("contracts", ["getContractData"]),
+    getNames() {
+      let data = this.getContractData({
+        contract: "Guestbook",
+        method: "getNames"
+      });
+      if (data === "loading") return false;
+      return data
+    },
+    utils() {
+      return this.drizzleInstance.web3.utils
+    }
+  },
+  created() {
+    this.$store.dispatch("drizzle/REGISTER_CONTRACT", {
+      contractName: "Guestbook",
+      method: "getNames",
+      methodArgs: []
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
